65a6300295ea2e0a084a12751dcc616e7dc0c646
foransic <fsouin@gmail.com>
2014-11-27 08:40:57 +0100

    Project initialization

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-badjs
-=====
+# Badj's 
 
-Badj's - add instantly &amp; easily social badges to your pages
+**add instantly &amp; easily social badges to your websites**
+
+Badj's is a jQuery plugin that eases badges integration in your websites.
+
+## Get started
+
+1. Download CSS & JS files & include it in your web page
+2. Make sure you include jQuery API (v1.10 or further)
+3. Include a list element which will contains badges, for example :
+
+
+```html
+<ul class="badjs">
+<li data-badj="mail" data-url="mailto:test@example.com"></li>
+</ul>
+```
+
+4. Include the javascript that will initiate badges, for example :
+
+```javascript
+$(document).ready(function() {
+
+	$('.badjs li').each(function() {
+
+		$(this).badjIt();
+
+	});
+
+});
+```
+
+## Examples
+
+Example is better than precept, you can find a working example in docs/ folder.
+
+
---
+++ src/js/jquery.badjs.js
@@ -0,0 +1,54 @@
+badjs = {
+		'fb' : {
+			'title': 'Facebook',
+			'image' : 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA9gDnANAUSQpzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3gccDCAKMETXEAAAAUBJREFUaN7tmL1KA0EUhc/dTSDRaKFIqlSCtY3gG/gG1voyPoivItYpLFOEkC5FMIL4M5hjM8Ky7CTNTpiJ54Nl/wYu3929c3cHEEIIIQRsl8FIdmsxaWYui0yRvCX5TPKd5Edl+yK5yEGgR/KRm3nLQeSO22lNpIgkUQC4z77YvYgLJMpVkrgys9M2YnYiJagfkHgC8ABg6ZP4k3p9nAdq4iZWzGLHji43kXXoYcUS6bT4Ol0CKL3EKDDsguSqMtGszWyc1KxFcgLg2IuUAM4ahi0BfPvjEsDMzK5Sm7WGAI62jDmpnU9yLfY6030RmSdX7AAGDTNUUw2++v0hgJfUG+Iw0BCvc+sjg8D1g33p7JCIRCQiEYlIRCIS+Ucisda1/hYZPivXehsWJdL5Z699xhuAbsMtZ2aEEEIIIeLwC+E01a3W2JWLAAAAAElFTkSuQmCC',
+			'color' : '#3c568d', 
+			'ocolor': '#333333'
+		},
+		'linkedin' : {
+			'title': 'LinkedIn',
+			'image' : 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA9gDnANAUSQpzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3gccDCELXljWxwAAAcpJREFUaN7tmD9rFEEYh5/feXJEEJJOjWlSJKQSg11ALCwstAwI2gvJFwhWIuQz+AUUawkE0UokoDYWNpFACgmIhQRJkPy9n4V7sKw7IXey3q68Tznzzuw87LvvzgwEQRAEQ0CDDrTdAjqAJe010t72qO1Htj/Yfml7vqkiz/0nd5omMWl7q0Tk1TDX1RpgzFHi2zpoYmo9KXkj15so0rG9YHvN9gvbc40rv1nZbQFdSd2srQ0g6SgXdxZw8XmSDgvzKZfi7s35L0TuAveA40LXN0kPcrKvgZ1CzHngpiTbHgemgWvARPZP+gq8Bd5J2q06rR67nC+5mDNO07G9bPtTor9re8X2hX7W1R7AJfXqD085fhO4eEI2CLgNrAKzVZbfv+XSKVP6qu2HdRbph6VeIam7yDqwcUL/CHCrziLPgBFJM5KmgBvAdmJ9V+oq8lTS/fzWX9Ib4H3iwx+vq8hiov0gIXKuriL9/uhG6yqiKuLrXn4rPY+ESIiESIiESIj8dyKDHHV/Aj/4fVGXJ78Nd3YkLm7Nx0puVnrsAN8T7ZXcolzObj2KC9qX9DGLETBXItsG1iS5ZN7pTLTItqTPBEEQBEEQBEPjF30tKKNINnQ9AAAAAElFTkSuQmCC',
+			'color': '#1b86be',
+			'ocolor': '#333333'
+		},
+		'twitter' : {
+			'title': 'Twitter',
+			'image' : 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA9gDnANAUSQpzAAAACXBIWXMAAbrqAAG66gHB8Tn1AAAAB3RJTUUH3gccDCMKG2mE0wAAAjhJREFUaN7tmLtrVUEQh7+9JgaDaCRBsPGBYOOrSCeIghGEFIJCauMLQQsV/Qu0EGzTBCwEO01hYQpTRRRU0CLRgIWCGuMTQoT4Csn9bFYIco05yd2biPs1e+CcYee3M3t2ZiGTyWQymUwmk/kLYSEmVY8BbcAqYAi4HkK4X+G7UgihnMqJZnXpHG3b1XEr06M2qnXqGrVbPZBKxAq1Tz1T0C6oHeqkMzOg3ovPvSlTYr06qk6plwvYNalvnD3X1B3qSbU5hZBNUYRx7FVXzsLulMX4GsfOVBHZECMynddqm7p8BrvbBYVMqKdTplaT2v+HyfvUo+rGCnZPCgq5UNS3ukL/6hDG1B5gJ1D67fVeYA8wrA4Dd4DHwCAwVmS9gE+1Ogf6TccP9VBRn0oFBaxVrwBdwPNE6zQBjCZNrRj2duBIwoB/AV4ljQjwDriVOHPHgadJhYQQJoFuYCShkLshhKlabfZ96otEm7211hVsi3pCHVLLVRLxoOZlvDoQy/DVQEOV1md7CGGw1hHprHJKXVUXpD9CvVQlES8rlTa1FLJEPaiOzENEWd2/aHpmdat6cw5Czi0WAcvU3eqNggJG1cO/OsiaXT6o9fEv9RloATbHKngX0Ao0Fph3EDgeQni4UKvfob6NvffEtG5xtkypZ9XSYkilBvV8vCj4oH6bwfHv6sd4aF5UG1P5FeYpahuwBVgXD8b6WCGXgffAMPAshPDon7qgm5YyhhDM95+ZTCaTyWT+c34CBSsH6sKI0MkAAAAASUVORK5CYII=',
+			'color': '#00acee',
+			'ocolor': '#333333'
+		},
+		'mail' : {
+			'title': 'Mail',
+			'image' : 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA9gDnANAUSQpzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3gccDCEw71M/4wAAAlZJREFUaN7tmL9LHEEUx2c1YsDCIo0WCcKBIOYQRGy0ELQULeyiKHKCtlYWlhFs/RcEsbMQmwgW/gU2prGJvxLbwEW8EOU+ad7B8Nhb77yZveZ9YVjmzXe+b7+7j5nZdc5gMBgMBoPBEAtJM2Tgs3Nuodl5bwDOuaMkSb7HUYcCcEd83AGFuI8K5oByRBNlYC7WzX9S/c2IRjazcrdq5BCYUrGtCCa2VI4p4DCkkRPgLzDoxbqAvYAm9oAuT39Qcp6ENHIsyW6BPi/eC5wFMHEG9Hq6fZIL4DiGEYBvQI83NgBctWDiChjw9HokB7GNABxIPPGW5fIbV6iC0jpQnKhGAHYVZxR4acLECzCqNHZTeFGNVOW6pngl4LkBE89ASc1dU9q5vZFaacwo7k6dG/JjO2rOTEZp5mIE4DfQr/j7Gfx9xe0XDdptBOAX0K3mnKfwzhWnW+bSqpGOgKfoIRX74py79PqXEvMxlMNJuuE38kOO92nzisAfacV6nwai0dbS+gmMKO5H1Z8Gpl/hjIhWW4w8AJOKtyRj8xla88JZUvFJ0czVyDUwpnbkZeBRxv8Bsyk6szKGcJeVxpho52LkBhhW4xspu/q9X3ZSPvcpu/uG0hqWHFGNPADjamy1zuYHcAF8kHZRh1MFVpXmuFdmQY2cytOcUKWwCDy9sg8cScvCE7CotCck52lII9u15dNLVAIqAT+sKrXzl5ejCGyHNNKp+usRv9nXs3KHMpQAKzn8DlqpvZVG8a5JL++lfXXOVSMdJDq8PBX7h2owGAwGg8HQfvwH3gjofj45BVAAAAAASUVORK5CYII=',
+			'color': '#d54938',
+			'ocolor': '#333333'
+		},
+		'phone' : {
+			'title': 'Phone',
+			'image' : 'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA9gDnANAUSQpzAAAACXBIWXMAAA3XAAAN1wFCKJt4AAAAB3RJTUUH3gccDCIbaMKVYAAAAY5JREFUaN7tmL8rhVEYx7+HojsgAxsLsoiBDYsBWWSwuauBzBb/ARn9AUbdUkw2G8qPsgl1MyhukW7dDPKxXCWc83pT3vvofNbn1Hs+73Oe5zzvK0UikUgkEon8D4AeYBuoAEVgEchZkxgEbvjKJlBnRaLXI0E1OzNZ7S3tG9yQ1OGJ5SSNWhEZSYj3AS0WRF4S4gOS2iyIlBPi7ZI6LYhc/WBNgwWR84R4SdKlBZHDUHeWtOKcu7Zyj/hYznJvaTPyKOnME7u1JFKStO+JLVmbtSaAp2+OVhkYtyZz7KmTAtBoSWQqUPR5a1kpBKbgUUsiTcCDR+bZmsws8BLITN5XM0AXMAmMAd1Ac5Yi9cAaYbY+dzNgHrj/sOYO2ANWgWnAZSW0myBTBo6AOWAZeA2srVQ/m3O1KpOWxaxEHLAeqJm0FLOumdlAN0tDpVZac+GXIju1NgGceGazEDdAT60OmuvA6Q8lBtM+w/1lM6j+YWmVNCRpWFK/pG5J75fhgaQF59yFIpFIJBKJROzzBhFepCTSva1aAAAAAElFTkSuQmCC',
+			'color': '#60c63e',
+			'ocolor': '#333333'
+		}
+	};
+	
+jQuery.fn.extend({
+	badjIt: function() {
+		/* then we get data from current badje */
+		data = badjs[$(this).data('badj')];
+
+		/* we construct the badje */	
+		$(this).css('cursor', 'pointer');
+		$(this).css('background-image', 'url("data:image/png;base64,' + data['image'] + '")');
+		$(this).css('background-color', data['color']);
+		
+		/* we catch event on it */
+		$(this).on('mouseover', function() {
+			$(this).css('background-color', badjs[$(this).data('badj')]['ocolor']);
+		}).on('mouseout', function() {
+			$(this).css('background-color', badjs[$(this).data('badj')]['color']);
+		}).on('click', function() {
+			location.href = $(this).data('url');
+		});
+	}
+});
+
